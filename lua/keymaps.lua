26e355656ec94d82135c3a63d7770c0fe9e457a3
janey <jyxu7695@163.com>
2026-07-22 22:39:57 +0800

    refactor(keymaps): resize keys from <C-hjkl> to <S-hjkl>
    Window resize now <space>HJKL (= <space>Shift+hjkl), which is more
    ergonomic alongside the <space>hjkl window switch.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,13 +8,13 @@ local leaderKey = '<space>'
 vks('i','jj','<esc>',opts)
 vks('n',leaderKey..'ec',':vsplit $MYVIMRC<CR>',opts)
 vks('n',leaderKey..'sc',':source $MYVIMRC<CR>',opts)
-vks('n',leaderKey..'<C-j>',':resize -2<CR>',opts)
-vks('n',leaderKey..'<C-k>',':resize +2<CR>',opts)
-vks('n',leaderKey..'<C-l>',':vertical resize -2<CR>',opts)
-vks('n',leaderKey..'<C-h>',':vertical resize +2<CR>',opts)
+vks('n',leaderKey..'J',':resize -2<CR>',opts)
+vks('n',leaderKey..'K',':resize +2<CR>',opts)
+vks('n',leaderKey..'L',':vertical resize -2<CR>',opts)
+vks('n',leaderKey..'H',':vertical resize +2<CR>',opts)
 vks('n','<space><tab>',':tabNext<CR>',opts)
 
--- 窗口切换：<space>hjkl（和 resize 的 <space><C-hjkl> 互补不冲突）
+-- 窗口切换：<space>hjkl（和 resize 的 <space>HJKL 互补不冲突，小写切窗、大写调大小）
 vks('n',leaderKey..'h','<C-w>h',opts)
 vks('n',leaderKey..'j','<C-w>j',opts)
 vks('n',leaderKey..'k','<C-w>k',opts)
