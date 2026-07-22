9faf776e4e708c717d4cd5cec2052a28f198ae4d
janey <jyxu7695@163.com>
2026-07-22 22:41:32 +0800

    tweak(keymaps): resize step 2→5 for snappier feel

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,10 +8,10 @@ local leaderKey = '<space>'
 vks('i','jj','<esc>',opts)
 vks('n',leaderKey..'ec',':vsplit $MYVIMRC<CR>',opts)
 vks('n',leaderKey..'sc',':source $MYVIMRC<CR>',opts)
-vks('n',leaderKey..'J',':resize -2<CR>',opts)
-vks('n',leaderKey..'K',':resize +2<CR>',opts)
-vks('n',leaderKey..'L',':vertical resize -2<CR>',opts)
-vks('n',leaderKey..'H',':vertical resize +2<CR>',opts)
+vks('n',leaderKey..'J',':resize -5<CR>',opts)
+vks('n',leaderKey..'K',':resize +5<CR>',opts)
+vks('n',leaderKey..'L',':vertical resize -5<CR>',opts)
+vks('n',leaderKey..'H',':vertical resize +5<CR>',opts)
 vks('n','<space><tab>',':tabNext<CR>',opts)
 
 -- 窗口切换：<space>hjkl（和 resize 的 <space>HJKL 互补不冲突，小写切窗、大写调大小）
